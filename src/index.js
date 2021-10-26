5882e2265a78f3e0fde4a9aff3f8517f1655c9e9
Luiz <luizhc2309@hotmail.com>
2021-10-26 08:11:12 -0300

    correções

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./controler/authControler')(app);
+require('./controllers/authController')(app);
 
 app.listen(3000);
