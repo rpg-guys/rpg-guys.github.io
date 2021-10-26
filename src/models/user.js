ba52a001892e1562aca77125f86299d82333e9b5
Luiz <luizhc2309@hotmail.com>
2021-10-26 08:20:57 -0300

    correção senha

--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
     },
-    passwords: {
+    password: {
         type: String,
         required: true,
         select: false,
