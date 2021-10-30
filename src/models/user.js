a58b61ed0134a4b8896947a42922bd564ca28216
Luiz <luizhc2309@hotmail.com>
2021-10-30 13:46:04 -0300

    correção servidor

--- src/models/user.js
+++ src/models/user.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-UserSchema.pre('save', function(next){
+UserSchema.pre('save', async function(next){
 
     // voltar aqui await
     const hash = await bcrypt.hash(this.password, 10);
